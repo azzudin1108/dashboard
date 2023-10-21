c084334a7f4a490ffa6e933f82dbb83f5c75312e
azzudin1108 <82307614+azzudin1108@users.noreply.github.com>
2023-10-21 17:20:45 +0700

    Add files via upload

--- dashboard/script/script.js
+++ dashboard/script/script.js
@@ -1,65 +1,44 @@
-const nav = document.getElementsByTagName('a');
-const primary = 'rgb(13,110,253)';
-const tertiary = 'rgb(248,249,250)';
-const menu = document.getElementById('menu');
-const dropdown = document.querySelector('#navbar ul');
+const sidebar = document.getElementById('sidebar');
+const iconSb = document.querySelectorAll('.icon-sb');
 
-// navigation menu
-const manage = document.getElementById('manage');
-const list = document.getElementById('list');
-const pengumuman = document.getElementById('pengumuman');
-const help = document.getElementById('help');
-manage.classList.add('showing');
-
-for (let i = 0; i < 4; i++) {
-    nav[0].style.backgroundColor = tertiary;
-    nav[0].style.color = primary;
-    nav[i].addEventListener('mouseenter', function () {
-        nav[i].classList.add('hover');
-    });
-    nav[i].addEventListener('mouseleave', function () {
-        nav[i].classList.remove('hover');
-    });
-    nav[i].addEventListener('click', function (e) {
-        e.target.style.backgroundColor = tertiary;
-        e.target.style.color = primary;
+sidebar.addEventListener('click', function (e) {
+    if (e.target.classList.item('icon-sb')) {
         e.preventDefault();
-        if (i == 0) {
-            nav[1].removeAttribute('style');
-            nav[2].removeAttribute('style');
-            nav[3].removeAttribute('style');
-            manage.classList.add('showing');
-            list.classList.remove('showing');
-            pengumuman.classList.remove('showing');
-            help.classList.remove('showing');
-        } else if (i == 1) {
-            nav[0].removeAttribute('style');
-            nav[2].removeAttribute('style');
-            nav[3].removeAttribute('style');
-            manage.classList.remove('showing');
-            list.classList.add('showing');
-            pengumuman.classList.remove('showing');
-            help.classList.remove('showing');
-        } else if (i == 2) {
-            nav[1].removeAttribute('style');
-            nav[0].removeAttribute('style');
-            nav[3].removeAttribute('style');
-            manage.classList.remove('showing');
-            list.classList.remove('showing');
-            pengumuman.classList.add('showing');
-            help.classList.remove('showing');
-        } else if (i == 3) {
-            nav[1].removeAttribute('style');
-            nav[2].removeAttribute('style');
-            nav[0].removeAttribute('style');
-            manage.classList.remove('showing');
-            list.classList.remove('showing');
-            pengumuman.classList.remove('showing');
-            help.classList.add('showing');
+        if (e.target.classList.contains('bi')) {
+            for (let i = 0; i < iconSb.length; i++) {
+                iconSb[i].style.color = 'white';
+            }
+            if (e.target.classList.contains('bi-house')) {
+                e.target.parentElement.style.color = 'rgb(37, 7, 94)';
+                e.target.parentElement.parentElement.firstElementChild.style.height = '50px';
+                e.target.parentElement.parentElement.lastElementChild.style.top = '107.5px';
+                e.target.parentElement.parentElement.lastElementChild.previousElementSibling.style.top = '50px';
+                e.target.parentElement.parentElement.lastElementChild.previousElementSibling.previousElementSibling.style.top = '50px';
+            } else if (e.target.classList.contains('bi-person-gear')) {
+                e.target.parentElement.style.color = 'rgb(37, 7, 94)';
+                e.target.parentElement.parentElement.firstElementChild.style.height = '107.5px';
+                e.target.parentElement.parentElement.lastElementChild.style.top = '165px';
+                e.target.parentElement.parentElement.lastElementChild.previousElementSibling.style.top = '107.5px';
+                e.target.parentElement.parentElement.lastElementChild.previousElementSibling.previousElementSibling.style.top = '107.5px';
+            } else if (e.target.classList.contains('bi-bell')) {
+                e.target.parentElement.style.color = 'rgb(37, 7, 94)';
+                e.target.parentElement.parentElement.firstElementChild.style.height = '165px';
+                e.target.parentElement.parentElement.lastElementChild.style.top = '222.5px';
+                e.target.parentElement.parentElement.lastElementChild.previousElementSibling.style.top = '165px';
+                e.target.parentElement.parentElement.lastElementChild.previousElementSibling.previousElementSibling.style.top = '165px';
+            } else if (e.target.classList.contains('bi-chat')) {
+                e.target.parentElement.style.color = 'rgb(37, 7, 94)';
+                e.target.parentElement.parentElement.firstElementChild.style.height = '222.5px';
+                e.target.parentElement.parentElement.lastElementChild.style.top = '280px';
+                e.target.parentElement.parentElement.lastElementChild.previousElementSibling.style.top = '222.5px';
+                e.target.parentElement.parentElement.lastElementChild.previousElementSibling.previousElementSibling.style.top = '222.5px';
+            } else if (e.target.classList.contains('bi-gear')) {
+                e.target.parentElement.style.color = 'rgb(37, 7, 94)';
+                e.target.parentElement.parentElement.firstElementChild.style.height = '280px';
+                e.target.parentElement.parentElement.lastElementChild.style.top = '337.5px';
+                e.target.parentElement.parentElement.lastElementChild.previousElementSibling.style.top = '280px';
+                e.target.parentElement.parentElement.lastElementChild.previousElementSibling.previousElementSibling.style.top = '280px';
+            }
         }
-    });
-}
-
-menu.addEventListener('click', function () {
-    dropdown.classList.toggle('show');
+    }
 });
